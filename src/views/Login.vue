4910273641d52946e20dfd7b7a1c9b465f826f8e
Fazd <fabioandres2907@hotmail.com>
2019-11-02 01:00:47 -0500

    first commit

---
+++ src/views/Login.vue
@@ -0,0 +1,82 @@
+<template>
+  <v-container fill-height>
+      <v-card class="mx-auto" width="500">
+        <v-toolbar color="primary" dark>
+            <v-toolbar-title>Inicio de sesion</v-toolbar-title>
+        </v-toolbar>
+        <v-card-text>
+            <v-form>
+                <v-text-field 
+                    prepend-icon="mdi-account-circle" 
+                    label="E-mail" 
+                    v-model.trim="emailInpt"
+                    v-on:input="validEmail"
+                    :error = "!enableEmail"
+                    >
+
+                </v-text-field>
+                
+                <v-text-field 
+                    :type= " showPassword ? 'text' : 'password'" 
+                    label="Contraseña"
+                    prepend-icon = "mdi-lock"
+                    :append-icon = "showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+                    @click:append="showPassword= !showPassword"
+                    v-model.trim="passwordInpt"
+                    :error ="!enablePassword"
+
+                >
+
+                </v-text-field>
+            </v-form>
+        </v-card-text>
+        <v-card-actions>
+            <v-btn color="success">Registrarse</v-btn>
+            <v-spacer></v-spacer>
+            <v-btn color="primary" @click="isValid">Login</v-btn>
+        </v-card-actions>
+      </v-card>
+
+  </v-container>
+</template>
+
+<script>
+export default {
+    data: ()=>({
+        emailInpt: '',
+        passwordInpt: '',
+        showPassword: false,
+        enableEmail: true,
+        enablePassword:true
+    }),
+    methods:{
+        validEmail(){
+            this.enableEmail = this.emailInpt.match(/\S+@\S+\.\S+/) != null
+        },
+        isValid(){
+            var op = this.emailInpt.match(/\S+@\S+\.\S+/) != null && this.passwordInpt.length > 2
+            if(op){
+                /*var text = JSON.stringify({
+                "Email":this.emailInpt,
+                "Password":this.passwordInpt
+                })
+                console.log(text)
+                */
+                this.enableEmail = true
+                this.enablePassword = true
+                
+            }
+            else{
+                alert("Complete los campos")
+                this.enableEmail = this.emailInpt.match(/\S+@\S+\.\S+/) != null
+                this.enablePassword = this.passwordInpt.length > 2
+            
+            } 
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
